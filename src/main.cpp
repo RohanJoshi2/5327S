7cfeddc32950da6cd9e243bc946e0c3b84646ef3
RohanJoshi2 <rohanjoshi7678@gmail.com>
2026-09-24 13:50:27 -0700

    Auton Functions

--- src/main.cpp
+++ src/main.cpp
@@ -20,7 +20,7 @@ controller Controller1 = controller(primary);
 // Misc Motors
 motor intakeMotor = motor(PORT11, false);
 motor clawMotor = motor(PORT7, true);
-motor clawLiftMotor = motor(PORT15, true);
+motor clawLiftMotor = motor(PORT15, false);
 
 // Lift motors
 motor liftLeft = motor(PORT8, false);
@@ -44,47 +44,81 @@ motor_group rightDrive = motor_group(rightMotor1, rightMotor2, rightMotor3);
 //
 
 // Intake
-void intake() {
-  if (Controller1.ButtonR1.pressing()) {
+void intake(bool in, bool out) {
+  if (in && !out) {
     intakeMotor.spin(forward, 100, percent);
     clawMotor.spin(forward, 100, percent);
-  } else if (Controller1.ButtonR2.pressing()) {
+  } else if (out && !in) {
     intakeMotor.spin(reverse, 100, percent);
     clawMotor.spin(reverse, 100, percent);
   } else {
-    intakeMotor.stop(hold);
+    intakeMotor.stop(coast);
     clawMotor.stop(hold);
   }
 }
 
+void intakeFor(bool in, bool out, int durationMs) {
+  intake(in, out);
+  wait(durationMs, msec);
+  intake(false, false);
+}
+
 // Lift
-void lift() {
-  if (Controller1.ButtonL1.pressing()) {
+void lift(bool up, bool down) {
+  if (up && !down) {
     liftMotors.spin(forward, 100, percent);
-  } else if (Controller1.ButtonL2.pressing()) {
+  } else if (down && !up) {
     liftMotors.spin(reverse, 100, percent);
   } else {
     liftMotors.stop(hold);
   }
 }
 
+void liftFor(bool up, bool down, int durationMs) {
+  lift(up, down);
+  wait(durationMs, msec);
+  lift(false, false);
+}
+
 // Claw Lift
 bool clawLiftActive = false;
-void clawLift() {
-  static bool buttonWasPressed = false;
-  bool buttonPressed = Controller1.ButtonA.pressing();
-  if (buttonPressed && !buttonWasPressed) {
-    clawLiftMotor.setVelocity(100, percent);
-    if (clawLiftActive) {
-      clawLiftMotor.spinFor(reverse, 200, degrees);
-      clawLiftActive = false;
-    } else {
-      clawLiftMotor.spinFor(forward, 200, degrees);
-      clawLiftActive = true;
-    }
-    clawLiftMotor.stop(hold);
+void clawLiftToggle() {
+
+  clawLiftMotor.setVelocity(100, percent);
+
+  if (clawLiftActive) {
+    clawLiftMotor.spinFor(reverse, 325, degrees);
+  } else {
+    clawLiftMotor.spinFor(forward, 325, degrees);
   }
-  buttonWasPressed = buttonPressed;
+
+  clawLiftActive = !clawLiftActive;
+  clawLiftMotor.stop(hold);
+}
+
+// Arcade drive function
+void arcadeDrive() {
+  int throttle = 0.7*Controller1.Axis3.position();
+  int turn = 0.7*Controller1.Axis1.position();
+
+  int leftPower = throttle - turn;
+  int rightPower = throttle + turn;
+
+  // Limit the calculated values to the motor's valid range
+  if (leftPower > 100)
+    leftPower = 100;
+
+  if (leftPower < -100)
+    leftPower = -100;
+
+  if (rightPower > 100)
+    rightPower = 100;
+
+  if (rightPower < -100)
+    rightPower = -100;
+
+  leftDrive.spin(forward, leftPower, percent);
+  rightDrive.spin(forward, rightPower, percent);
 }
 
 // PID function
@@ -154,33 +188,8 @@ void drivePID(double targetMotorDeg) {
     elapsed += LOOP_TIME;
   }
 
-  leftDrive.stop(hold);
-  rightDrive.stop(hold);
-}
-
-// Arcade drive function
-void arcadeDrive() {
-  int throttle = 0.7*Controller1.Axis3.position();
-  int turn = 0.7*Controller1.Axis1.position();
-
-  int leftPower = throttle - turn;
-  int rightPower = throttle + turn;
-
-  // Limit the calculated values to the motor's valid range
-  if (leftPower > 100)
-    leftPower = 100;
-
-  if (leftPower < -100)
-    leftPower = -100;
-
-  if (rightPower > 100)
-    rightPower = 100;
-
-  if (rightPower < -100)
-    rightPower = -100;
-
-  leftDrive.spin(forward, leftPower, percent);
-  rightDrive.spin(forward, rightPower, percent);
+  leftDrive.stop(coast);
+  rightDrive.stop(coast);
 }
 
 /*---------------------------------------------------------------------------*/
@@ -202,12 +211,20 @@ void autonomous(void) {
 /*---------------------------------------------------------------------------*/
 
 void usercontrol(void) {
+  bool previousA = false;
 
   while (1) {
+    bool currentA = Controller1.ButtonA.pressing();
+
+    if (currentA && !previousA) {
+      clawLiftToggle();
+    }
+
+    previousA = currentA;
+
     arcadeDrive();
-    intake();
-    lift();
-    clawLift();
+    intake(Controller1.ButtonR1.pressing(), Controller1.ButtonR2.pressing());
+    lift(Controller1.ButtonL1.pressing(), Controller1.ButtonL2.pressing());
 
     wait(20, msec);
   }
